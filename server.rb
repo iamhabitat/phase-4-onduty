416f588107e5147d66318254a538559e936752c8
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-04-08 19:40:43 -0700

    prettified styling

--- server.rb
+++ server.rb
@@ -2,18 +2,27 @@ require 'sinatra'
 require 'haml'
 require 'trello'
 
-TRELLO_KEY = "46bf7a717a5e6ed3ed5be94e5448358b";
-TRELLO_TOKEN = "f769b642237eb2ed7006a7ca7e9b187e1f9dc4194dcca431bddf7b8e185bb363";
-#TRELLO_SECRET = "18db07004aa21155610108fb959e6b6a306f2555f90b17195290d3360720c51c";
+enable :sessions
 
 Trello.configure do |config|
-  config.developer_public_key = TRELLO_KEY
-  config.member_token = TRELLO_TOKEN
+  config.developer_public_key = ENV['TRELLO_KEY']
+  config.member_token = ENV['TRELLO_TOKEN']
+end
+
+
+before do
+  unless ENV['TRELLO_KEY'] and ENV['TRELLO_TOKEN']
+    raise "You need to set ENV['TRELLO_KEY'] and ENV['TRELLO_TOKEN']!"
+  end
 end
 
 
 get '/' do
-  @board = Trello::Board.find("5150ea649171af5e5b009c64")
+  unless session[:cached_board]# and ((session[:cached_board][0] - Time.now) / 300) > 1
+    puts "updating board"
+    session[:cached_board] = [Time.now, Trello::Board.find("5150ea649171af5e5b009c64")]
+  end
+  @board = session[:cached_board][1]
 
   haml :index
 end
