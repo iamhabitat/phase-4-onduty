247563a729c173364ae64a2cf568c26975b1b689
Myles Byrne <myles@myles.id.au>
2013-06-06 19:39:17 -0700

    Route that uses mock data, tweaked the design a little

--- server.rb
+++ server.rb
@@ -1,49 +1,35 @@
 require 'sinatra'
 require 'haml'
-require 'trello'
-
-require './current_timeslot'
-include CurrentTimeslot
 
 require './perlisms'
+require './timeslots'
+require './mock_timeslots'
 
 # TODO: visually highlight current timeslot
 
-# Read in the trello config from the environment. Halts if any config value is
-# missing.
-TRELLO_CONF = %w(TRELLO_KEY TRELLO_TOKEN TRELLO_BOARD).
-  reduce({}) do |conf, vname|
-    abort "Please set #{vname} in the environment" unless ENV.has_key? vname
-    conf_key = vname.split('_').last.downcase.to_sym
-    conf.merge! conf_key => ENV[vname]
-  end
+get '/' do
+  headers 'X-Frame-Options' => "GOFORIT"
+  haml :day
+end
 
-Trello.configure do |config|
-  config.developer_public_key = TRELLO_CONF[:key]
-  config.member_token = TRELLO_CONF[:token]
+# For hacking on the UI
+get '/mock' do
+  @mock = true
+  haml :day
 end
 
 helpers do
 
   def todays_timeslots
-    board.lists.find { |l| l.name == current_day_of_week }.try(:cards) or []
+    @mock ? MockTimeslots.load : Timeslots.load
   end
 
   def current_day_of_week
     Time.now.strftime("%A")
   end
 
-  def board
-    @board ||= Trello::Board.find TRELLO_CONF[:board]
-  end
-
   def perlisms
     Perlisms
   end
 
-end
-
-get '/' do
-  headers 'X-Frame-Options' => "GOFORIT"
-  haml :day
 end
